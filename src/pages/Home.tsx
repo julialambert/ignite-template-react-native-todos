b55182ded3adbe40b6dfb9bcc102586f23293f42
Júlia Lambert <julialambert@MacBook-Pro-de-Julia.local>
2022-06-20 17:22:45 -0300

    feat: finishing the challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, task]);
+
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+    const itemFound = updatedTasks.find(item => item.id === id);
+    if(!itemFound)
+      return;
+    
+    itemFound.done = !itemFound.done
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
